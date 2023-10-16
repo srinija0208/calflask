045b0648c72145e12bca8ab0f61925c9adb6829a
srinija0208 <srinijasomannagari@gmail.com>
2023-10-16 13:29:07 +0530

    code added

--- app.py
+++ app.py
@@ -5,7 +5,29 @@ obj=Flask(__name__)
 @obj.route('/')
 def welcome():
     return "welcome to flask"
+
+@obj.route('/cal',methods=['GET'])
+def math_operator():
+    operation=request.json["operation"]
+    number1=request.json["number1"]
+    number2=request.json["number2"]
+
+    ## json is a form of writing the data ( similarly, YMAL,XML ...... )
+
+
+    if operation == "add":
+        result=int(number1)+int(number2)
+    elif operation  == "sub":
+        result=int(number1)-int(number2)
+    elif operation == "mul":
+        result=int(number1)*int(number2)
+    else:
+        result=int(number1)/int(number2)
+
+    return "the operation is {} and the result is {}".format(operation,result)
+
 print(__name__)
+
 
 if __name__=="__main__":
     obj.run()
